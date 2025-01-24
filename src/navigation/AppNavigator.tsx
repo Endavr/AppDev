839f932d049deb0fea0be9bf741c58ecdd2e4124
Yna Cabanado <yna.cabanado@gmail.com>
2025-01-24 15:40:17 +0800

    signup, login, edit working

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -7,49 +7,57 @@ import TasksScreen from '../screens/TasksScreen';
 import AddTaskScreen from '../screens/AddTaskScreen';
 import EditTaskScreen from '../screens/EditTaskScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import SignupScreen from '../screens/SignUpScreen';
+import LoginScreen from '../screens/LoginScreen';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-// In AppNavigator.tsx
 const TasksStack = () => (
   <Stack.Navigator>
-    {/* <Stack.Screen name="TasksList" component={TasksScreen} options={{ title: 'Tasks' }} />*/}
-    <Stack.Screen name="AddTaskScreen" component={AddTaskScreen} options={{ title: 'Add Task' }} />
-    {/* <Stack.Screen name="EditTaskScreen" component={EditTaskScreen} options={{ title: 'Edit Task' }} />*/}
+    {/* <Stack.Screen name="TasksList" component={TasksScreen} options={{ title: 'Tasks' }} /> */}
+    <Stack.Screen name="AddTask" component={AddTaskScreen} options={{ title: 'Add Task' }} />
+    {/* <Stack.Screen name="EditTask" component={EditTaskScreen} options={{ title: 'Edit Task' }} /> */}
   </Stack.Navigator>
 );
 
+const MainTabNavigator = () => (
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ color, size }) => {
+        let iconName: string;
+
+        switch (route.name) {
+          case 'Home':
+            iconName = 'home';
+            break;
+          case 'Tasks':
+            iconName = 'list';
+            break;
+          case 'Settings':
+            iconName = 'settings';
+            break;
+          default:
+            iconName = 'circle';
+        }
+
+        return <MaterialIcons name={iconName} size={size} color={color} />;
+      },
+    })}
+  >
+    <Tab.Screen name="Home" component={HomeScreen} />
+    <Tab.Screen name="Tasks" component={TasksStack} />
+    <Tab.Screen name="Settings" component={SettingsScreen} />
+  </Tab.Navigator>
+);
 
 const AppNavigator = () => {
   return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          let iconName: string;
-
-          switch (route.name) {
-            case 'Home':
-              iconName = 'home';
-              break;
-            case 'Tasks':
-              iconName = 'list';
-              break;
-            case 'Settings':
-              iconName = 'settings';
-              break;
-            default:
-              iconName = 'circle';
-          }
-
-          return <MaterialIcons name={iconName} size={size} color={color} />;
-        },
-      })}
-    >
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Tasks" component={TasksStack} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-    </Tab.Navigator>
+    <Stack.Navigator initialRouteName="Signup">
+      <Stack.Screen name="Signup" component={SignupScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Main" component={MainTabNavigator} options={{ headerShown: false }} />
+    </Stack.Navigator>
   );
 };
 
